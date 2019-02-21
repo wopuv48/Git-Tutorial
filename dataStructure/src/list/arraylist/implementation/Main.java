19cb634cf413d351f473e498f86b28792d62036e
wopuv48 <bbmm98@hanmail.net>
2019-02-21 14:27:30 +0900

    Add Java File [dataStructure]

---
+++ dataStructure/src/list/arraylist/implementation/Main.java
@@ -0,0 +1,18 @@
+package list.arraylist.implementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList numbers = new ArrayList();
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addLast(40);
+		
+		ArrayList.ListIterator li = numbers.listIterator();
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+	}
+}
